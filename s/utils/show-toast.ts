5f59693cda5dd988fe97e0b627869bf5c35c47c9
vinas1221 <benjo_vinas@yahoo.com>
2025-01-05 08:19:57 +0000

    Update API - enhancing logs

--- s/utils/show-toast.ts
+++ s/utils/show-toast.ts
@@ -1,6 +1,6 @@
 // styles for it are inside index.css
 export function showToast(message: string, type: "error" | "warning" | "info") {
-	const toast = document.createElement("div")
+	let toast = document.createElement("div")
 	toast.textContent = message
 	toast.className = `toast ${type}`
 	document.body.appendChild(toast)
