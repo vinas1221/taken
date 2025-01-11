46cdb70d367bb95d32d8186f240de84bba92ffd7
vinas1221 <benjo_vinas@yahoo.com>
2025-01-11 10:10:44 +0000

    Remove build - adding new feature

--- s/utils/compare_arrays.ts
+++ s/utils/compare_arrays.ts
@@ -1,15 +1,15 @@
 import {AnyEffect} from "../context/types.js"
 
 export function compare_arrays(originalArray: AnyEffect[], newArray: AnyEffect[]) {
-	const add = []
-	const remove = []
+	let add = []
+	let remove = []
 
-	for (const item of newArray) {
+	for (let item of newArray) {
 		if (!originalArray.find(({id}) => id === item.id)) {
 			add.push(item)
 		}
 	}
-	for (const item of originalArray) {
+	for (let item of originalArray) {
 		if (!newArray.find(({id}) => id === item.id)) {
 			remove.push(item)
 		}
