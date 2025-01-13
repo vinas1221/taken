dc10b559d8aa12e4e8006a05572530f309cbb6e5
vinas1221 <benjo_vinas@yahoo.com>
2025-01-13 17:55:57 +0000

    Document README - improving security

--- s/get-components.ts
+++ s/get-components.ts
@@ -2,7 +2,7 @@ import {OmniText} from "./components/omni-text/component.js"
 import {OmniMedia} from "./components/omni-media/component.js"
 import {OmniTimeline} from "./components/omni-timeline/component.js"
 
-export const getComponents = () => ({
+export let getComponents = () => ({
 	OmniText,
 	OmniTimeline,
 	OmniMedia
