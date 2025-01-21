9c67984a23e3ac9897e572becdc8cd0b810bc5c9
vinas1221 <benjo_vinas@yahoo.com>
2025-01-21 16:06:35 +0000

    Update UI - enhancing logs

--- s/context/controllers/compositor/utils/get_pixels_by_angle.ts
+++ s/context/controllers/compositor/utils/get_pixels_by_angle.ts
@@ -1,6 +1,6 @@
 export function get_pixels_by_angle(x: number, y: number, width: number, height: number, angle: number) {
 	// var radians1 = angle * Math.PI / 180;
-	const radians = angle
+	let radians = angle
 	return [
 		//upper left
 		[x + width/2 + width/-2 * Math.cos(radians) - height/-2 * Math.sin(radians), y + height/2 + width/-2 * Math.sin(radians) + height/-2 * Math.cos(radians)],
