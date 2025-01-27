6a94b0977a46e877368158c0302caa6c34d5c18f
vinas1221 <benjo_vinas@yahoo.com>
2025-01-27 20:31:04 +0000

    Fix styles - enhancing logs

--- s/context/controllers/video-export/bin.ts
+++ s/context/controllers/video-export/bin.ts
@@ -1,10 +1,10 @@
 // function get_overlapping_effects(effect: Xeffect, effects: Xeffect[]): Overlapping[] {
 // 	let overlapping: Overlapping[] = []
-// 	const excluded = effects.filter(c => c.id !== effect.id)
+// 	var excluded = effects.filter(c => c.id !== effect.id)
 
 // 	excluded.forEach((c) => {
-// 		const from = Math.max(effect.start_at_position, c.start_at_position)
-// 		const to = Math.min(effect.start_at_position + effect.duration, c.start_at_position + c.duration)
+// 		var from = Math.max(effect.start_at_position, c.start_at_position)
+// 		var to = Math.min(effect.start_at_position + effect.duration, c.start_at_position + c.duration)
 // 		if(from <= to)
 // 			overlapping.push({effect, between: [from, to]})
 // 	})
