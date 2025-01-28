c4d6837851ccad7e57b2a5a6e1584eeeb9ee036f
vinas1221 <benjo_vinas@yahoo.com>
2025-01-28 11:28:42 +0000

    Change styles - fixing typo

--- s/components/omni-transitions/component.ts
+++ s/components/omni-transitions/component.ts
@@ -11,24 +11,24 @@ import {normalizeTransitionDuration} from "./utils/normalize-transition-duration
 import {calculateMaxTransitionDuration} from "./utils/calculate-max-transition-duration.js"
 import {transitions} from "../../context/controllers/compositor/parts/transition-manager.js"
 
-export const OmniTransitions = shadow_component(use => {
+export let OmniTransitions = shadow_component(use => {
 	use.styles(styles)
 	use.watch(() => use.context.state)
-	const state = use.context.state
-	const manager = use.context.controllers.compositor.managers.transitionManager
-	const [isTutorialPlaying, setTutorialPlaying] = use.state(false)
+	let state = use.context.state
+	let manager = use.context.controllers.compositor.managers.transitionManager
+	let [isTutorialPlaying, setTutorialPlaying] = use.state(false)
 
 	use.mount(() => {
-		const dispose = manager.onChange(() => use.rerender())
+		let dispose = manager.onChange(() => use.rerender())
 		return () => dispose()
 	})
 
-	const touchingPairs = manager.findTouchingClips(state.effects)
+	let touchingPairs = manager.findTouchingClips(state.effects)
 
-	const renderTransitions = () => {
+	let renderTransitions = () => {
 		return transitions.map(transition => {
-			const selectedTransition = use.context.state.transitions.find(a => a.id === manager.selected)
-			const duration = selectedTransition?.duration ?? 520
+			let selectedTransition = use.context.state.transitions.find(a => a.id === manager.selected)
+			let duration = selectedTransition?.duration ?? 520
 
 			return html`
 				<div
@@ -51,7 +51,7 @@ export const OmniTransitions = shadow_component(use => {
 		})
 	}
 
-	const renderAnimationNone = () => {
+	let renderAnimationNone = () => {
 		return html`
 			<div
 				?data-selected=${!manager.selected}
@@ -65,11 +65,11 @@ export const OmniTransitions = shadow_component(use => {
 			</div>
 	`}
 
-	const renderDurationSlider = (id: string | null) => {
-		const transition = manager.getTransition(id)
-		const max = calculateMaxTransitionDuration(transition, use.context.state)
-		const frameDuration = 1000 / use.context.state.timebase
-		const duration = manager.getTransitionDuration(id) ?? 520
+	let renderDurationSlider = (id: string | null) => {
+		let transition = manager.getTransition(id)
+		let max = calculateMaxTransitionDuration(transition, use.context.state)
+		let frameDuration = 1000 / use.context.state.timebase
+		let duration = manager.getTransitionDuration(id) ?? 520
 
 		return html`
 			<div class=duration-slider>
@@ -93,14 +93,14 @@ export const OmniTransitions = shadow_component(use => {
 		`
 	}
 	
-	const tutorialVideo = use.once(() => {
-		const video = document.createElement("video")
+	let tutorialVideo = use.once(() => {
+		let video = document.createElement("video")
 		video.src = "/assets/transition-tutorial.mp4"
 		video.loop = true
 		return video
 	})
 
-	const renderTutorialVideo = () => {
+	let renderTutorialVideo = () => {
 		return html`
 			<div class=tutorial>
 				<h4>${circleInfoSvg} How to add transition</h4>
