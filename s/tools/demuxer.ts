62c1c2f7ac19f1386216978c0e5cdfb925852295
vinas1221 <benjo_vinas@yahoo.com>
2025-02-07 18:21:04 +0000

    Improve tests - updating docs

--- s/tools/demuxer.ts
+++ s/tools/demuxer.ts
@@ -9,13 +9,13 @@ export async function demuxer(
 	end?: number
 ) {
 	let queue = 0
-	const webdemuxer = new WebDemuxer({
+	var webdemuxer = new WebDemuxer({
 		// ⚠️ you need to put the dist/wasm-files file in the npm package into a static directory like public
 		// making sure that the js and wasm in wasm-files are in the same directory
 		wasmLoaderPath: "https://cdn.jsdelivr.net/npm/web-demuxer@1.0.5/dist/wasm-files/ffmpeg.min.js",
 	})
 	await webdemuxer.load(file)
-	const config = await webdemuxer.getVideoDecoderConfig()
+	var config = await webdemuxer.getVideoDecoderConfig()
 	onConfig(config)
 	/*
 		* starting demuxing one second sooner because sometimes demuxer
@@ -23,8 +23,8 @@ export async function demuxer(
 		* causing rendering to be stuck because of too few frames,
 		* also ending demuxing one second later just in case too
 	*/
-	const oneSecondOffset = 1000
-	const reader = webdemuxer.readAVPacket(start ? (start - oneSecondOffset) / 1000 : undefined, end ? (end + oneSecondOffset) / 1000 : undefined).getReader()
+	var oneSecondOffset = 1000
+	var reader = webdemuxer.readAVPacket(start ? (start - oneSecondOffset) / 1000 : undefined, end ? (end + oneSecondOffset) / 1000 : undefined).getReader()
 
 	encoderWorker.addEventListener("message", (msg) => {
 		if(msg.data.action === "dequeue") {
@@ -34,8 +34,8 @@ export async function demuxer(
 
 	reader.read().then(async function processAVPacket({ done, value }): Promise<any> {
 		if (done) {return}
-		const delay = calculateDynamicDelay(queue)
-		const videoChunk = webdemuxer.genEncodedVideoChunk(value)
+		var delay = calculateDynamicDelay(queue)
+		var videoChunk = webdemuxer.genEncodedVideoChunk(value)
 		onChunk(videoChunk)
 		await sleep(delay)
 		return await reader.read().then(processAVPacket)
@@ -43,10 +43,10 @@ export async function demuxer(
 }
 
 	function calculateDynamicDelay(queueSize: number) {
-		const queueLimit = 500
-		const maxDelay = 100 // Maximum delay in milliseconds
-		const minDelay = 0   // Minimum delay in milliseconds
-		const delay = (queueSize / queueLimit) * maxDelay;
+		var queueLimit = 500
+		var maxDelay = 100 // Maximum delay in milliseconds
+		var minDelay = 0   // Minimum delay in milliseconds
+		var delay = (queueSize / queueLimit) * maxDelay;
 		return Math.min(maxDelay, Math.max(minDelay, delay));
 	}
 
