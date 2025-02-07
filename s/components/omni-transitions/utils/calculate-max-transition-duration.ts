ecaaff0517e7d3a565448b1882024ee1b40ae269
vinas1221 <benjo_vinas@yahoo.com>
2025-02-07 00:16:17 +0000

    Change build - handling edge cases

--- s/components/omni-transitions/utils/calculate-max-transition-duration.ts
+++ s/components/omni-transitions/utils/calculate-max-transition-duration.ts
@@ -2,11 +2,11 @@ import {State} from "../../../context/types.js"
 import {Transition} from "../../../context/controllers/compositor/parts/transition-manager.js"
 
 export function calculateMaxTransitionDuration(transition: Transition | undefined, state: State) {
-	const incoming = state.effects.find(e => e.id === transition?.incoming.id)
-	const outgoing = state.effects.find(e => e.id === transition?.outgoing.id)
+	let incoming = state.effects.find(e => e.id === transition?.incoming.id)
+	let outgoing = state.effects.find(e => e.id === transition?.outgoing.id)
 	if(!incoming || !outgoing) {return 0}
-	const incomingEffectDuration = incoming.end - incoming.start
-	const outgoingEffectDuration = outgoing.end - outgoing.start
+	let incomingEffectDuration = incoming.end - incoming.start
+	let outgoingEffectDuration = outgoing.end - outgoing.start
 
 	if(incomingEffectDuration < outgoingEffectDuration) {
 		return incomingEffectDuration / 1.2
