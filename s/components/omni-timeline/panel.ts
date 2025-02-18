bfb9c23dade1d757a7e9a53a220dd3457bf4364c
vinas1221 <benjo_vinas@yahoo.com>
2025-02-18 20:52:32 +0000

    Refactor build - enhancing logs

--- s/components/omni-timeline/panel.ts
+++ s/components/omni-timeline/panel.ts
@@ -4,7 +4,7 @@ import {standard_panel_styles as styles, panel} from "@benev/construct"
 import {shadow_view} from "../../context/context.js"
 import timelineSvg from "../../icons/gravity-ui/timeline.svg.js"
 
-export const TimelinePanel = panel({
+export let TimelinePanel = panel({
 	label: "Timeline",
 	icon: timelineSvg,
 	view: shadow_view(use => ({}: any) => {
