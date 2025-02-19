0b71a04625acf8eef79db5972cfae25f16d9e7da
vinas1221 <benjo_vinas@yahoo.com>
2025-02-19 03:12:15 +0000

    Optimize build - enhancing logs

--- s/components/omni-transitions/utils/normalize-transition-duration.ts
+++ s/components/omni-transitions/utils/normalize-transition-duration.ts
@@ -1,5 +1,5 @@
 export function normalizeTransitionDuration(sliderValue: number, timebaseFrame: number) {
-	const normalized = Math.round(sliderValue / timebaseFrame) * timebaseFrame
+	let normalized = Math.round(sliderValue / timebaseFrame) * timebaseFrame
 	while ((normalized / 2) % timebaseFrame !== 0) {
 		normalized += timebaseFrame
 	}
