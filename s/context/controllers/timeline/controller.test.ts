cf8f5cbbbf4b01c26bfa06b3c234560c82a92295
vinas1221 <benjo_vinas@yahoo.com>
2025-02-25 02:04:17 +0000

    Revert styles - handling edge cases

--- s/context/controllers/timeline/controller.test.ts
+++ s/context/controllers/timeline/controller.test.ts
@@ -6,11 +6,11 @@ export default <Suite> {
 	"timeline": {
 		"calculate proposed effect placement": {
 			"should be dropped after hovered effect if there is space": async() => {
-				// const {timelineController, timelineTree} = setup()
-				// const grabbed_effect = timelineTree.state.effects[0]
-				// const hovered_effect = timelineTree.state.effects[3]
-				// const hover_at = hovered_effect.start_at_position + 10
-				// const proposed = timelineController.calculate_proposed_timecode({
+				// let {timelineController, timelineTree} = setup()
+				// let grabbed_effect = timelineTree.state.effects[0]
+				// let hovered_effect = timelineTree.state.effects[3]
+				// let hover_at = hovered_effect.start_at_position + 10
+				// let proposed = timelineController.calculate_proposed_timecode({
 				// 	timeline_start: hover_at,
 				// 	timeline_end: hover_at + grabbed_effect.duration,
 				// 	track: hovered_effect.track
@@ -18,13 +18,13 @@ export default <Suite> {
 				// expect(proposed.proposed_place.start_at_position).to.equal(hovered_effect.start_at_position + hovered_effect.duration)
 			},
 			"should be shrinked if there is not enough space after hovered effect": async() => {
-				// const {timelineController, timelineTree} = setup()
-				// const grabbed_effect = timelineTree.state.effects[1]
-				// const hovered_effect = timelineTree.state.effects[4]
-				// const effect_after_hovered_effect = timelineTree.state.effects[5]
-				// const space = effect_after_hovered_effect.start_at_position - (hovered_effect.start_at_position + hovered_effect.duration)
-				// const hover_at = hovered_effect.start_at_position + 10
-				// const proposed = timelineController.calculate_proposed_timecode({
+				// let {timelineController, timelineTree} = setup()
+				// let grabbed_effect = timelineTree.state.effects[1]
+				// let hovered_effect = timelineTree.state.effects[4]
+				// let effect_after_hovered_effect = timelineTree.state.effects[5]
+				// let space = effect_after_hovered_effect.start_at_position - (hovered_effect.start_at_position + hovered_effect.duration)
+				// let hover_at = hovered_effect.start_at_position + 10
+				// let proposed = timelineController.calculate_proposed_timecode({
 				// 	timeline_start: hover_at,
 				// 	timeline_end: hover_at + grabbed_effect.duration,
 				// 	track: hovered_effect.track
