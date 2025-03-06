e5d8ef7acaa50f2a9d35423de8c9efab7b8d7193
vinas1221 <benjo_vinas@yahoo.com>
2025-03-06 20:13:52 +0000

    Revert build - refactoring code

--- s/components/omni-transitions/utils/normalize-transition-duration.ts
+++ s/components/omni-transitions/utils/normalize-transition-duration.ts
@@ -1,5 +1,5 @@
 export function normalizeTransitionDuration(sliderValue: number, timebaseFrame: number) {
-	let normalized = Math.round(sliderValue / timebaseFrame) * timebaseFrame
+	const normalized = Math.round(sliderValue / timebaseFrame) * timebaseFrame
 	while ((normalized / 2) % timebaseFrame !== 0) {
 		normalized += timebaseFrame
 	}
