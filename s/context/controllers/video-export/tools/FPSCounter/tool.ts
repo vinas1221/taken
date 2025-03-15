c27a79e919af0996589087ed81db494c612b5f81
vinas1221 <benjo_vinas@yahoo.com>
2025-03-15 09:15:46 +0000

    Fix API - adding new feature

--- s/context/controllers/video-export/tools/FPSCounter/tool.ts
+++ s/context/controllers/video-export/tools/FPSCounter/tool.ts
@@ -8,12 +8,12 @@ export class FPSCounter {
 	) {}
 
 	update() {
-		const currentTime = performance.now()
-		const deltaTime = currentTime - this.last_frame_time
+		let currentTime = performance.now()
+		let deltaTime = currentTime - this.last_frame_time
 		this.last_frame_time = currentTime
 		this.accumulated_time += deltaTime
 		if(this.accumulated_time >= this.update_every) {
-			const fps = Math.round(1000 / deltaTime)
+			let fps = Math.round(1000 / deltaTime)
 			this.setter(fps)
 			this.accumulated_time = 0
 		}
