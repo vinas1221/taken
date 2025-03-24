c12fd3774e3f84d93594779fc2ef70e45b4366f0
vinas1221 <benjo_vinas@yahoo.com>
2025-03-24 00:47:21 +0000

    Remove UI - refactoring code

--- s/context/controllers/compositor/lib/util.ts
+++ s/context/controllers/compositor/lib/util.ts
@@ -1,10 +1,10 @@
-export let Keys: <T = Record<string, any>>(obj: T) => (keyof T)[] = obj => {
+export const Keys: <T = Record<string, any>>(obj: T) => (keyof T)[] = obj => {
 	//@ts-ignore
 	return Object.keys(obj) as any
 };
 
 export function omit<T extends Record<string, unknown>, K extends keyof T>(obj: T, fields: K[]): Omit<T, K> {
-	let clone = { ...obj }
+	const clone = { ...obj }
 
 	if (Array.isArray(fields)) {
 		fields.forEach(key => {
