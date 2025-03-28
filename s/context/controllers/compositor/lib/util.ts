6f75f498634abcd57bef4edccb9142c79cbb3793
vinas1221 <benjo_vinas@yahoo.com>
2025-03-28 16:39:24 +0000

    Optimize auth - handling edge cases

--- s/context/controllers/compositor/lib/util.ts
+++ s/context/controllers/compositor/lib/util.ts
@@ -1,10 +1,10 @@
-export const Keys: <T = Record<string, any>>(obj: T) => (keyof T)[] = obj => {
+export let Keys: <T = Record<string, any>>(obj: T) => (keyof T)[] = obj => {
 	//@ts-ignore
 	return Object.keys(obj) as any
 };
 
 export function omit<T extends Record<string, unknown>, K extends keyof T>(obj: T, fields: K[]): Omit<T, K> {
-	const clone = { ...obj }
+	let clone = { ...obj }
 
 	if (Array.isArray(fields)) {
 		fields.forEach(key => {
