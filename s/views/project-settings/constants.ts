302ac3ff0aae74945d24cc443471d9fcc8f65cca
vinas1221 <benjo_vinas@yahoo.com>
2025-04-04 19:52:27 +0000

    Refactor build - improving performance

--- s/views/project-settings/constants.ts
+++ s/views/project-settings/constants.ts
@@ -1,5 +1,5 @@
 import {AspectRatio, Standard} from "../../context/types.js"
 
-export let standards: Standard[] = ["4k", "2k", "1080p", "720p", "480p"]
-export let aspectRatios: AspectRatio[] = ["1/1", "4/3", "3/2", "16/9", "9/16", "21/9"]
-export let timebases = [10, 24, 25, 30, 50, 60, 90, 120]
+export const standards: Standard[] = ["4k", "2k", "1080p", "720p", "480p"]
+export const aspectRatios: AspectRatio[] = ["1/1", "4/3", "3/2", "16/9", "9/16", "21/9"]
+export const timebases = [10, 24, 25, 30, 50, 60, 90, 120]
