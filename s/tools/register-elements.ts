f5a5edcd82bab3512e48c79c6a8f9beb2df606bf
vinas1221 <benjo_vinas@yahoo.com>
2025-04-17 02:54:10 +0000

    Refactor API - adding new feature

--- s/tools/register-elements.ts
+++ s/tools/register-elements.ts
@@ -4,8 +4,8 @@ export function registerElements(elements: {
 		[name: string]: {new(): HTMLElement}
 	}) {
 
-	for (const [name, element] of Object.entries(elements)) {
-		const dashified = dashify(name)
+	for (let [name, element] of Object.entries(elements)) {
+		let dashified = dashify(name)
 		if(!customElements.get(dashified))
 			customElements.define(dashified, element)
 	}
